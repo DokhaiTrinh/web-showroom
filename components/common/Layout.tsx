89b9687c7c72439515ad7ef6facc4801b8c01699
maitanchinh <maitanchinh@gmail.com>
2022-07-23 23:12:47 +0700

    service

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import Footer from "./Footer";
-import NavBar from "./NavBar";
+import NavBar from "./NavBar/index";
 
 type Props = {
   children: ReactNode;
