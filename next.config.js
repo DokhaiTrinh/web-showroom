5963ee2087cf2ed3db184ba80c1a32643381dbf7
DESKTOP-INDD2I0\khanh <khanhduyx2@gmail.com>
2022-07-23 11:32:46 +0700

    Remove material ui lib and unused code in home page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["niemtinvang.vn"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
