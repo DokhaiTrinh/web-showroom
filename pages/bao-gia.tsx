b9c5e409a4843ea9781bf6cf24e72c433cefb9e0
maitanchinh <maitanchinh@gmail.com>
2022-07-24 23:17:02 +0700

    pricing

--- pages/bao-gia.tsx
+++ pages/bao-gia.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from '../styles/Pricing.module.css';
+import Link from "next/link";
 
 function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+          <h2 className={styles.title}>BẢNG TÍNH DỰ TRÙ CHI PHÍ XÂY DỰNG</h2>
+          <p>Nếu cần cập nhật thêm, xin liên hệ Hoàng Sơn qua zalo <Link href="/"><a className={styles.phone}>0909 805 931</a></Link> để có thông tin chi tiết.</p>
+          <iframe width="100%" height="600" scrolling="no" src="https://onedrive.live.com/embed?resid=F0833C89E6A7849C%21180379&authkey=%21APVtplLclbO_3YE&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
